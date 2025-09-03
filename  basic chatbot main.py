ea10cb4f15ad86519ab58444715243d5bde2ef18
siddhukasar193-cmyk <siddhukasar193@gmail.com>
2025-09-03 16:55:53 +0530

    Add files via upload

---
+++  basic chatbot main.py
@@ -0,0 +1,20 @@
+# Basic Chatbot
+
+def chatbot():
+    print("🤖 Chatbot: Hello! Type 'bye' to exit.")
+    
+    while True:
+        user_input = input("You: ").lower()   # take input and convert to lowercase
+        
+        if user_input == "hello":
+            print("🤖 Chatbot: Hi!")
+        elif user_input == "how are you":
+            print("🤖 Chatbot: I'm fine, thanks!")
+        elif user_input == "bye":
+            print("🤖 Chatbot: Goodbye! Have a nice day!")
+            break
+        else:
+            print("🤖 Chatbot: Sorry, I don't understand that.")
+
+# Run the chatbot
+chatbot()
